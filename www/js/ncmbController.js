d28d32fa23ff3d12e4ceb31edb871d6c3661a973
k18rs058 <k18rs058@st.kyusan-u.ac.jp>
2019-11-25 14:22:31 +0900

    スコア送信処理を実装した

--- www/js/ncmbController.js
+++ www/js/ncmbController.js
@@ -1,16 +1,37 @@
 // ncmbController.js
 
 var ncmbController = {
-  APPLICATION_KEY:appkey,
-  CLIENT_KEY:clientkey,
+  APPLICATION_KEY:appKey,
+  CLIENT_KEY:clientKey,
 
   ncmb:null,
   currentUser:null,
   screenSize:null,
 
-  init:function(screenSize){
+  //スコア送信処理
+  sendScore: function(score) {
     var self = this;
-    self.ncmb=new NCMB(self.APPLICATION_KEY,self.CLIENT_KEY);
-    selfscreenSize = screenSize;
-  }
+
+    // [1]Score（クラス）を生成
+    var Score = self.ncmb.DataStore("ScoreClass");
+
+    // [2]インスタンス生成、スコア数値をフィールド名"score"にセット
+    var scoreData = new Score({score: score});
+
+    // [3]送信処理
+    scoreData.save()
+        .then(function (saved) {
+            alert("スコア送信完了！");
+        })
+       .catch(function(err){
+            console.log(err);
+        });
+},
+
+
+  init: function(screenSize) {
+        var self = this;
+        self.ncmb = new NCMB(self.APPLICATION_KEY, self.CLIENT_KEY);    // mobile backendの初期化
+        self.screenSize = screenSize;
+    }
 }
